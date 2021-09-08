104c3eda7e0cb242c07f4807be87e486f80c4829
Alex <alexurban12@gmail.com>
2021-09-08 11:24:50 +0200

    Layout finalized, to add element functionality

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function addBookToLibrary() {
 const read = document.querySelector(".button");
 read.addEventListener('click', e => {
     e.target.textContent == 'READ' ? e.target.textContent = 'UNREAD' : e.target.textContent = 'READ';
-})
+    e.target.textContent == 'READ' ? e.target.style.background = 'rgb(117, 96, 96)' : e.target.style.background = 'rgb(100, 62, 62)' ;
+  })
 
 const addItem = document.querySelector(".add-item");
 addItem.addEventListener('click', e => {
